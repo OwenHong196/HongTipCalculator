5d639f2941924d367f68dbaac38ea8f0abca8b89
BT_2N2_26 <sample@bths.edu>
2023-09-21 13:33:32 -0400

    items

--- HongTipCalculator.java
+++ HongTipCalculator.java
@@ -5,7 +5,7 @@ public class HongTipCalculator {
         Scanner scan = new Scanner(System.in);
         System.out.print("Welcome Summoner!");
         System.out.print("How many people are in your group: ");
-        String numPeople = scan.nextLine();
+        int numPeople = scan.nextInt();
         System.out.print("What's the tip percentage? (0-100): ");
         int tipPercentage = scan.nextInt();
         System.out.print("Enter a cost in dollars and cents (-1 to end): ");
@@ -16,13 +16,21 @@ public class HongTipCalculator {
             System.out.print("Enter a cost in dollars and cents (-1 to end): ");
             bill = bill + scan.nextDouble();
             System.out.print("Enter the item: ");
-            item = scan.nextLine();
+            item = item.add(scan.nextLine());
         }
-        double total = bill * tipPercentage / 100;
+
+        double total = bill * tipPercentage / 100 + bill;
+        double tip = bill * tipPercentage / 100;
         System.out.println("-----------------------------");
-        System.out.println("Total bill before tip: " + bill);
+        System.out.println("Total bill before tip: $" + bill);
         System.out.println("Total percentage: " + tipPercentage + "%");
-        System.out.println("Total tip: " + total);
-        System.out.println("Total tip: " + )
+        System.out.println("Total tip: $" + tip);
+        System.out.println("Total bill with tip: $" + total);
+        System.out.println("Per person cost before tip: $" + (bill / numPeople));
+        System.out.println("Tip per person: $" + (tip / numPeople));
+        System.out.println("Total cost per person: $" + (total / numPeople));
+        System.out.println("-----------------------------");
+        System.out.println("Items ordered: ");
+
     }
 }
